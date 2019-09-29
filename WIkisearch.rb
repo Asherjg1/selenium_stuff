51c544562c7bc1a1a2a1d9b290329ca702ee9dec
Asherjg1 <asherjg1@gmail.com>
2019-09-29 15:02:01 -0700

    created a wiki selenium game

---
+++ WIkisearch.rb
@@ -0,0 +1,17 @@
+require 'selenium-WebDriver'
+@browser=Selenium::WebDriver.for :chrome
+@browser.navigate.to"https://en.wikipedia.org/wiki/Tom_Brady"
+sleep 3
+@browser.find_element(:xpath => "//a[@href='/wiki/Brazil']").click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Japanese_Brazilian"]').click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Japanese_language"]').click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Japan"]').click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Tokyo"]').click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Anime"]').click
+sleep 3
+@browser.find_element(:xpath => '//a[@href="/wiki/Pok%C3%A9mon"]').click
